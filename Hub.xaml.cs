9b81580c67b5ba8b18fe37d4f3fb7994f7acbb1d
Roodri02 <82373420+Roodri02@users.noreply.github.com>
2022-04-27 12:42:34 +0200

    menu y hub

--- Hub.xaml.cs
+++ Hub.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Imaging;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -52,11 +54,65 @@ namespace P3JoseMiguelVillacanas
             Frame.Navigate(typeof(Nivel1));
         }
 
+        private void Level4_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Nivel1));
+        }
+
         private void RobotTeam_Click(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(RobotTeam));
         }
 
+ 
 
+        private void n1Img_PointerEntered_1(object sender, PointerRoutedEventArgs e)
+        {
+            n1Img.Width +=25;
+            n1Img.Height += 25;
+        }
+
+        private void n1Img_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            n1Img.Width -=25;
+            n1Img.Height -=25;
+        }
+
+        private void n3Img_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            n3Img.Width += 25;
+            n3Img.Height += 25;
+        }
+
+        private void n2Img_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            n2Img.Width += 25;
+            n2Img.Height += 25;
+        }
+
+        private void n2Img_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            n2Img.Width -= 25;
+            n2Img.Height -= 25;
+        }
+
+        private void n3Img_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            n3Img.Width -= 25;
+            n3Img.Height -= 25;
+
+        }
+
+        private void n4Img_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            n4Img.Width += 25;
+            n4Img.Height += 25;
+        }
+
+        private void n4Img_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            n4Img.Width -= 25;
+            n4Img.Height -= 25;
+        }
     }
 }
--- Nivel1.xaml.cs
+++ Nivel1.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -20,8 +22,12 @@ namespace P3JoseMiguelVillacanas
     /// <summary>
     /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
     /// </summary>
+    /// 
+
     public sealed partial class Nivel1 : Page
     {
+        public ObservableCollection<RobotVM> ListaRobots { get; } = new ObservableCollection<RobotVM>();
+
         public Nivel1()
         {
             this.InitializeComponent();
@@ -32,5 +38,35 @@ namespace P3JoseMiguelVillacanas
             Frame.Navigate(typeof(Pause));
         }
 
+        private async void Image_DropAsync(object sender, DragEventArgs e)
+        {
+            var id = await e.DataView.GetTextAsync();
+            var number = int.Parse(id);
+            //Point PD = e.GetPosition(MiCanvas);
+            //MiDron.Source = ListaDrones[number].Img.Source;
+            //MiImagen.Source = ListaDrones[number].Img.Source;
+            //Point pos = e.GetPosition(mi_mapa);
+
+            //MiDron.Visibility = Visibility.Visible;
+            //Texto.Text = ListaDrones[number].Explicacion;
+            //Sel = int.Parse(id);
+
+            //ListaRobots[Sel].Transformacion.TranslateX = pos.X;
+            //ListaRobots[Sel].Transformacion.TranslateY = pos.Y;
+            //MiDronCC.RenderTransform = ListaDrones[Sel].Transformacion;
+        }
+
+        private void Image_DragOver(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = DataPackageOperation.Copy;
+        }
+
+        private void ImageGridView_DragItems(object sender, DragItemsStartingEventArgs e)
+        {
+            RobotVM d = e.Items[0] as RobotVM;
+            string id = d.Id.ToString();
+            e.Data.SetText(id);
+            e.Data.RequestedOperation = DataPackageOperation.Copy;
+        }
     }
 }
